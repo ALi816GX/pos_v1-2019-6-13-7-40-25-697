28356eca127e0f58596c9a1b28b18b4f7916fbc4
linguangxiong <854501715@qq.com>
2019-07-14 12:32:45 +0800

    feature:add function isBarCodesExist and isBarcodeExist

--- main/main.js
+++ main/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
-//TODO: 请在该文件中实现练习要求并删除此注释
+
+const isBarCodeExist = (barcode) => {
+
+    const newbarcode = barcode.split("-")[0];
+    const itemDatas = loadAllItems();
+
+    let result = false;
+    itemDatas.forEach(item => {
+        if(newbarcode == item["barcode"]){
+            result =  true;
+        }
+    });
+
+    return result;
+};
+
+
+const isBarCodesExist = (barcodes) => {
+
+    if(barcodes.length == 0){
+        return false;
+    }
+
+    for(let barcode of barcodes){
+        if(!isBarCodeExist(barcode)){
+            return false;
+        }
+    }
+
+    return true;
+};
+
--- test/main-test.js
+++ test/main-test.js
@@ -1,33 +1,119 @@
 'use strict';
 
-describe('pos', () => {
 
-  it('should print text', () => {
+const barcodes = [
+    'ITEM000001',
+    'ITEM000001',
+    'ITEM000001',
+    'ITEM000001',
+    'ITEM000001',
+    'ITEM000003-2',
+    'ITEM000005',
+    'ITEM000005',
+    'ITEM000005'
+];
 
-    const tags = [
-      'ITEM000001',
-      'ITEM000001',
-      'ITEM000001',
-      'ITEM000001',
-      'ITEM000001',
-      'ITEM000003-2.5',
-      'ITEM000005',
-      'ITEM000005-2',
-    ];
+const barcodes1 = [
+];
 
-    spyOn(console, 'log');
+const barcodes2 = [
+    'ITEM0000011',
+    'ITEM000001',
+    'ITEM000001',
+    'ITEM000001',
+    'ITEM000001',
+    'ITEM000003-2',
+    'ITEM000005',
+    'ITEM000005',
+    'ITEM000005'
+];
 
-    printReceipt(tags);
 
-    const expectText = `***<没钱赚商店>收据***
-名称：雪碧，数量：5瓶，单价：3.00(元)，小计：12.00(元)
-名称：荔枝，数量：2.5斤，单价：15.00(元)，小计：37.50(元)
-名称：方便面，数量：3袋，单价：4.50(元)，小计：9.00(元)
-----------------------
-总计：58.50(元)
-节省：7.50(元)
-**********************`;
+describe('Test function isBarCodesExist',()=>{
+        it('should return true',()=>{
 
-    expect(console.log).toHaveBeenCalledWith(expectText);
-  });
-});
+            const result = isBarCodesExist(barcodes);
+
+            expect(result).toBe(true);
+
+        })
+
+    it('should return false',()=>{
+
+        const result = isBarCodesExist(barcodes1);
+
+        expect(result).toBe(false);
+
+    })
+
+    it('should return false',()=>{
+
+        const result = isBarCodesExist(barcodes2);
+
+        expect(result).toBe(false);
+
+    })
+    }
+);
+
+
+// describe('method1',()=>{
+//         it('should return true',()=>{
+//
+//             const result = judge(2,1);
+//
+//             console.log(result);
+//
+//             expect(result).toBe(true);
+//
+//         })
+//     }
+// );
+//
+//
+// describe('method2',()=>{
+//         it('should return 3',()=>{
+//
+//             const result = add(2,1);
+//
+//             console.log(result);
+//
+//             expect(result).toBe(3);
+//
+//         })
+//     }
+// );
+
+
+// describe('pos', () => {
+
+//   it('should print text', () => {
+
+//     const tags = [
+//       'ITEM000001',
+//       'ITEM000001',
+//       'ITEM000001',
+//       'ITEM000001',
+//       'ITEM000001',
+//       'ITEM000003-2.5',
+//       'ITEM000005',
+//       'ITEM000005-2',
+//     ];
+
+//     spyOn(console, 'log');
+
+//     printReceipt(tags);
+
+//     const expectText = `***<没钱赚商店>收据***
+// 名称：雪碧，数量：5瓶，单价：3.00(元)，小计：12.00(元)
+// 名称：荔枝，数量：2.5斤，单价：15.00(元)，小计：37.50(元)
+// 名称：方便面，数量：3袋，单价：4.50(元)，小计：9.00(元)
+// ----------------------
+// 总计：58.50(元)
+// 节省：7.50(元)
+// **********************`;
+
+//     expect(console.log).toHaveBeenCalledWith(expectText);
+//   });
+
+// });
